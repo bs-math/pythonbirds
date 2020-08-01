2b99ba66fa6bb6a96c5826555454cf374a0a125b
bs_ma <descobriHA01>
2020-08-01 17:51:43 -0300

    Criados atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,18 @@
 class Pessoa:
-    def __init__(self, *filhos, nome = None, idade = 0):
+    olhos = 2 #atributo de classe
+
+    def __init__(self, *filhos, nome=None, idade=0): #atributos de instância
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    math = Pessoa(nome='Math', idade = 26)
-    luciano = Pessoa(math, nome='Luciano', idade = 80)
+    math = Pessoa(nome='Math', idade=26)
+    luciano = Pessoa(math, nome='Luciano', idade=80)
     print(Pessoa.cumprimentar(luciano))     #mostram que todos os
     print(id(luciano))                      #objetos usados aqui são
     print(luciano.cumprimentar())           #o mesmo objeto
@@ -19,3 +23,5 @@ if __name__ == '__main__':
     luciano.sobrenome = 'Ramalho'           #atributo dinânico
     print(luciano.__dict__)                 #mostra os atributos do objeto
     print(math.__dict__)
+    print(Pessoa.olhos) # Pode mostrar o atributo de classe, mas não os de instância
+    print(math.olhos)
